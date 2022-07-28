03406f41fefe57299ff978b001c7256c82eee175
falcaomm <manu.m.falcao@gmail.com>
2022-07-28 14:42:05 -0300

    Ex fixação

---
+++ a010-ex-fixacao/script.js
@@ -0,0 +1,17 @@
+let array = [7,5,9,3]
+console.log(array);
+
+array.sort()
+console.log(array);
+
+array.reverse()
+console.log(array);
+
+array.pop()
+console.log(array);
+
+array.push(6)
+console.log(array)
+
+array.splice(2,1)
+console.log(array);
